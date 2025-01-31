0e672542abf4f1f82624b92169d85fda8773c6d4
Joel <joelpercca3@gmail.com>
2025-01-30 20:48:55 -0500

    commit 2do gaurdado

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+    return (
+        <div className='descriptionbox'>
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Reviews(122)</div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>An e-commerce website is on online platform that facilitates the buying
+                    and selling of products or services the internet.It serves as a virtual marketplace
+                    where businesses and individuals can showcase their products, interact with
+                    customers, and conduct transactions without the need for a physical presence.
+                    E-commerce websites have gained immense popularity due to their conveniencie,
+                    accsebility, and the global reach they ofter.</p>
+                    E-commerce websites typically display products or services
+                    along with detailed descriptions,images,prices,and available variations(e.g.,sizes,colors)
+                    Each product usually has its own dedicated page with relevant information.
+                
+                <p>
+
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default DescriptionBox;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDom from "react-dom/client";
+import './index.css';
+import App from './App';
+import ShopContextProvider from "./Context/ShopContext";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+const root = ReactDom.createRoot(document.getElementById('root'));
+root.render(
+<ShopContextProvider>
+  <App/>
+</ShopContextProvider>
+  
+);
