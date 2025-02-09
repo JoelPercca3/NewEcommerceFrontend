145dedb2b4bea312f06321eaf2be9da083c703ad
Joel <joelpercca3@gmail.com>
2025-02-09 18:53:54 -0500

    Frontend tuto terminado

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // Si usas React, ajusta según tu stack
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // Cambiar el puerto aquí
+    port: 3000, // Configura el puerto 3000
   },
-});
+})
